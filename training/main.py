3f15299184477862beb8b915907a019663217cab
Jereml <Ledwojeremi@gmail.com>
2023-11-09 22:09:43 +0100

    Add wandb loging

--- training/main.py
+++ training/main.py
@@ -1,11 +1,37 @@
-import pytorch_lightning as pl
-from models.deeplab import Deeplab
+import wandb
 
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+from models.deeplab import Deeplab
+import glob
+from dataset import CustomDataset
+from torch.utils.data import DataLoader
+
+SAMPLES_DIR = "data_samples"
+
+def train_dataloader():
+    samples = glob.glob(SAMPLES_DIR+"/Train/*.npy")
+    dataset = CustomDataset(numpy_files=samples)
+    return DataLoader(dataset, batch_size=8, shuffle=True)
+
+def validation_dataloader():
+    samples = glob.glob(SAMPLES_DIR+"/Validation/*.npy")
+    dataset = CustomDataset(numpy_files=samples)
+    return DataLoader(dataset, batch_size=8, shuffle=True)
+
+def test_dataloader():
+    samples = glob.glob(SAMPLES_DIR+"/Test/*.npy")
+    dataset = CustomDataset(numpy_files=samples)
+    return DataLoader(dataset, batch_size=8, shuffle=True)
 
 def main():
+    wandb_logger = WandbLogger(project='car-segmentation')
+
     model = Deeplab(num_classes=9)
-    trainer = pl.Trainer(max_epochs=2)
-    trainer.fit(model)
+    trainer = pl.Trainer(max_epochs=2, logger=wandb_logger, log_every_n_steps=1)
+    trainer.fit(model, train_dataloader(), validation_dataloader())
+
+    wandb.finish()
 
 if __name__ == "__main__":
     main()
--- training/models/deeplab.py
+++ training/models/deeplab.py
@@ -1,19 +1,17 @@
 from matplotlib import pyplot as plt
 import pytorch_lightning as pl
 import torch
-from torch.utils.data import DataLoader
 import torch.nn.functional as F
 from torchvision.models.segmentation import deeplabv3_resnet50
-import glob
 
-from dataset import CustomDataset
 
-SAMPLES_DIR = "data_samples"
 
 class Deeplab(pl.LightningModule):
     def __init__(self, num_classes):
         super().__init__()
         self.model = deeplabv3_resnet50(num_classes=num_classes+1) # Adding background that is not included in the class number
+
+        self.save_hyperparameters()
 
     def forward(self, x):
         return self.model(x)['out']
@@ -28,65 +26,3 @@ class Deeplab(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
-
-    def train_dataloader(self):
-        samples = glob.glob(SAMPLES_DIR+"/Train/*.npy")
-        dataset = CustomDataset(numpy_files=samples)
-        return DataLoader(dataset, batch_size=4, shuffle=True)
-    
-    def test_dataloader(self):
-        samples = glob.glob(SAMPLES_DIR+"/Test/*.npy")
-        dataset = CustomDataset(numpy_files=samples)
-        return DataLoader(dataset, batch_size=4, shuffle=True)
-    
-
-    def on_train_end(self):
-        # Switch to eval mode
-        self.eval()
-        self.freeze()
-
-        # Get a sample batch from the validation dataset
-        val_loader = self.test_dataloader()
-        batch = next(iter(val_loader))
-        images, masks = batch
-        
-        # Perform inference
-        with torch.no_grad():
-            outputs = self(images)
-        
-        # Convert outputs to predicted class indices
-        _, preds = torch.max(outputs, 1)
-
-        # Plot the results
-        self.plot_sample(images[0], masks[0], preds[0])
-        
-        # Switch back to train mode
-        self.unfreeze()
-        self.train()
-
-    def plot_sample(self, image, mask, pred):
-        # Assuming image, mask, and pred are PyTorch tensors
-        # Normalize image for visualization if necessary
-        image = image.cpu().numpy().transpose((1, 2, 0))
-        mask = mask.cpu().numpy()
-        pred = pred.cpu().numpy()
-
-        # Create subplots
-        fig, ax = plt.subplots(1, 3, figsize=(15, 5))
-        
-        # Display the image
-        ax[0].imshow(image)
-        ax[0].set_title('Input Image')
-        ax[0].axis('off')
-
-        # Display the ground truth mask
-        ax[1].imshow(mask)
-        ax[1].set_title('Ground Truth Mask')
-        ax[1].axis('off')
-
-        # Display the predicted mask
-        ax[2].imshow(pred)
-        ax[2].set_title('Predicted Mask')
-        ax[2].axis('off')
-
-        plt.show()
