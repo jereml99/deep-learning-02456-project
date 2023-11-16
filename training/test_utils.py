eeef095948c25ec9da3286f6490108c013a20148
Jereml <Ledwojeremi@gmail.com>
2023-11-16 12:36:09 +0100

    Add better image log in wandb

--- training/test_utils.py
+++ training/test_utils.py
@@ -17,43 +17,61 @@ def calculate_iou(preds, labels):
         iou = intersection / union  # Calculate IoU
     return iou
 
-# Modify the test loop to include logging and metrics calculation
-import numpy as np
-import torch
-import wandb
+# Class labels dictionary
+class_labels = {
+    0: "No Car Element",
+    1: "Hood",
+    2: "Front Door",
+    3: "Rear Door",
+    4: "Frame",
+    5: "Rear Quarter Panel",
+    6: "Trunk Lid",
+    7: "Fender",
+    8: "Bumper",
+    9: "Rest of Car"
+}
+
+def preprocess_image(image_tensor):
+    # Convert tensor to PIL Image if necessary
+    return image_tensor
+
+def create_mask_dictionary(mask_tensor):
+    """
+    Create a dictionary for the mask with class descriptions.
+    :param mask_tensor: Tensor of the mask.
+    :return: Dictionary suitable for wandb.Image masks parameter.
+    """
+    mask_array = mask_tensor.numpy()
+    mask_dict = {
+        "mask_data": mask_array,
+        "class_labels": class_labels
+    }
+    return mask_dict
 
 def test_model(model, test_loader):
     model.eval()
     all_ious = []
-    test_images_list = []
-    ground_truth_list = []
-    prediction_list = []
-    
+
     with torch.no_grad():
-        for batch in test_loader:
-            images, masks = batch
+        for batch_idx, (images, masks) in enumerate(test_loader):
             predictions = model(images)
             preds = torch.argmax(predictions, dim=1)
-            
-            # Separate lists for test images, ground truths, and predictions
-            test_images_list.extend([wandb.Image(image.float()) for image in images])
-            ground_truth_list.extend([wandb.Image(mask.float()) for mask in masks])
-            prediction_list.extend([wandb.Image(pred.float()) for pred in preds])
-            
-            # Calculate IoU for the batch
-            iou = calculate_iou(preds, masks)
-            all_ious.append(iou)
-    
-    # Calculate mean IoU
-    mean_iou = np.mean(all_ious)
-    
-    # Log images under separate keys for a three-column layout
-    wandb.log({
-        "mean_test_iou": mean_iou,
-        "test": {"test_images": test_images_list,
-        "ground_truths": ground_truth_list,
-        "predictions": prediction_list}
 
-    })
+            for idx, (image, mask, pred) in enumerate(zip(images, masks, preds)):
+                vis_image = preprocess_image(image.float())
+                gt_mask_dict = create_mask_dictionary(mask)
+                pred_mask_dict = create_mask_dictionary(pred)
+
+                caption = f"Batch {batch_idx}, Image {idx}"
+                wandb.log({
+                    "test_image": wandb.Image(vis_image, 
+                                              caption=caption, 
+                                              masks={"ground_truth": gt_mask_dict, "prediction": pred_mask_dict})
+                })
+
+            all_ious.append(calculate_iou(preds, masks))
+
+    mean_iou = np.mean(all_ious)
+    wandb.log({"mean_test_iou": mean_iou})
 
     return mean_iou
