47a0874febdc3b5902acb2a6eb170930a9f1df1c
jereml99 <ledwojeremi@gmail.com>
2023-11-18 13:10:31 +0100

    Add the unet model

--- training/main.py
+++ training/main.py
@@ -8,6 +8,7 @@ from dataset import CustomDataset
 from torch.utils.data import DataLoader
 
 from test_utils import test_model
+from models.unet import UNet
 
 
 SAMPLES_DIR = "data_samples"
@@ -31,13 +32,14 @@ def test_dataloader():
 def main():
     wandb_logger = WandbLogger(project='car-segmentation')
 
-    model = Deeplab(num_classes=9)
-    trainer = pl.Trainer(max_epochs=1, logger=wandb_logger, log_every_n_steps=1)
+    model = UNet(in_channels=3, out_channels=10)
+    trainer = pl.Trainer(max_epochs=20, logger=wandb_logger, log_every_n_steps=1)
     trainer.fit(model, train_dataloader(), validation_dataloader())
 
 
     # Run testing after training
     mean_iou = test_model(model, test_dataloader())
+    
     print(f"Mean IoU on the test set: {mean_iou}")
     wandb.finish()
 
