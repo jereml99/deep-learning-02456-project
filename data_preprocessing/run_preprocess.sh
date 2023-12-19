96827741001c473377cf90d54ec6a449ed827f99
allutrifork <allu@trifork.com>
2023-12-19 16:44:48 +0100

    change of the paths in the bash script

--- data_preprocessing/run_preprocess.sh
+++ data_preprocessing/run_preprocess.sh
@@ -2,15 +2,15 @@
 
 #join all images
 echo "I am preprocessing all data with cropped landscapes, correct arrays"
-python3 ./deep-learning-02456-project/data_preparing.py
+python3 ./deep-learning-02456-project/data_preprocessing/data_preparing.py
 
 #synthetic dataset with 20 colours of synthetic cars, with arrays from Deloitte
 echo "I am preparing the first dataset"
-python3 ./deep-learning-02456-project/dataset_one.py
+python3 ./deep-learning-02456-project/data_preprocessing/dataset_one.py
 
 #synthetic dataset with 20 colours of synthetic cars, with arrays after the morphological closure
 echo "I am preparing the second dataset"
-python3 ./deep-learning-02456-project/dataset_two.py
+python3 ./deep-learning-02456-project/data_preprocessing/dataset_two.py
 
 
 
